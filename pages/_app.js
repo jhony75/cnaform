f471b535e23db4593bbd6ec039f0fd08669aa12e
Jônatas Lima de Medeiros <jonatas.lima.medeiros@live.com>
2024-06-08 09:55:41 -0300

    font changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
+import { Montserrat } from '@next/font/google';
+import '../styles/globals.css';
 
+const montserrat = Montserrat({ subsets: ['latin'] });
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
